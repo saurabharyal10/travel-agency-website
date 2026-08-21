d0c3da45a1b9790d5aca170639506994a7bf4cc3
Saurabh Aryal <saurabharyal10@gmail.com>
2026-08-21 17:04:58 +0545

    Remove fake duplicate featured-post block from blog page
    blog-featured.blade.php hardcoded a fake "The Himalayan Post" article
    disconnected from the real BlogPost model, shown right below blog-hero
    which already renders the actual featured post. Removed the component
    and its usage so the page shows one consistent, real featured post.

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,7 +2,6 @@
     <x-navbar />
     <x-blog-hero :post="$featuredPost" />
     <x-blog-stories :stories="$stories" />
-    <x-blog-featured />
     <x-blog-archive :archive="$archive" />
     <x-blog-cta />
     <x-footer />
